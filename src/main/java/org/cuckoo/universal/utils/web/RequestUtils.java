7fd3dba634feb96d66a48b39270d99fe357ae741
cuckoo20100401 <cuckoo20100401@163.com>
2019-08-01 12:58:14 +0800

    Modified RequestUtils.java

--- src/main/java/org/cuckoo/universal/utils/web/RequestUtils.java
+++ src/main/java/org/cuckoo/universal/utils/web/RequestUtils.java
@@ -12,11 +12,15 @@ public class RequestUtils {
 	
 	public static final String Range_Param_dateStart = "dateStart";
 	public static final String Range_Param_dateEnd = "dateEnd";
+	public static final String Range_Param_priceStart = "priceStart";
+	public static final String Range_Param_priceEnd = "priceEnd";
 
-	public static Map<String, Object> getRangeParams(HttpServletRequest request) {
-		Map<String, Object> rangeParams = new HashMap<>();
+	public static Map<String, String> getRangeParams(HttpServletRequest request) {
+		Map<String, String> rangeParams = new HashMap<>();
 		rangeParams.put(Range_Param_dateStart, request.getParameter(Range_Param_dateStart));
 		rangeParams.put(Range_Param_dateEnd, request.getParameter(Range_Param_dateEnd));
+		rangeParams.put(Range_Param_priceStart, request.getParameter(Range_Param_priceStart));
+		rangeParams.put(Range_Param_priceEnd, request.getParameter(Range_Param_priceEnd));
 		return rangeParams;
 	}
 }
