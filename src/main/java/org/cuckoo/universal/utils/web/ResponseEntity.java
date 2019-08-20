73f8c5cd722293e39cb3fe9d0336e20c139a18bc
cuckoo20100401 <cuckoo20100401@163.com>
2019-08-20 12:28:58 +0800

    upgraded ResponseEntity.java

--- src/main/java/org/cuckoo/universal/utils/web/ResponseEntity.java
+++ src/main/java/org/cuckoo/universal/utils/web/ResponseEntity.java
@@ -8,54 +8,113 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ResponseEntity {
 	
+	private Boolean success;
+	private String message;
+	private Integer code;
+	private Map<String, Object> data = new HashMap<>();
+	
 	public class Builder {
 		
-		private Map<String, Object> msg = new HashMap<>();
+		private ResponseEntity re = new ResponseEntity();
 		
 		public Builder success() {
-			msg.put("success", true);
+			this.re.success(true);
 			return this;
 		}
 		
 		public Builder failure() {
-			msg.put("success", false);
+			this.re.success(false);
 			return this;
 		}
 		
 		public Builder code(Integer code) {
-			msg.put("code", code);
+			this.re.code(code);
 			return this;
 		}
 		
 		public Builder message(String message) {
-			msg.put("message", message);
+			this.re.message(message);
 			return this;
 		}
 		
 		public Builder put(String key, Object value) {
-			msg.put(key, value);
+			this.re.put(key, value);
 			return this;
 		}
 		
-		public Builder put(Map<String, Object> msg) {
-			this.msg = msg;
-			return this;
+		public ResponseEntity as() {
+			return this.re;
 		}
-		
 		public Map<String, Object> asMap() {
-			return msg;
+			return this.re.asMap();
 		}
-		
+		public Map<String, Object> asMapWithout() {
+			return this.re.asMapWithout();
+		}
 		public String asJSON() throws JsonProcessingException {
-			return new ObjectMapper().writeValueAsString(msg);
+			return this.re.asJSON();
+		}
+		public String asJSONWithout() throws JsonProcessingException {
+			return this.re.asJSONWithout();
 		}
 	}
 	
 	public static Builder create() {
 		return new ResponseEntity().new Builder();
 	}
-	
 	public static Builder build() {
 		return create();
+	}
+
+	public Boolean success() {
+		return success;
+	}
+	public void success(Boolean success) {
+		this.success = success;
+	}
+	public String message() {
+		return message;
+	}
+	public void message(String message) {
+		this.message = message;
+	}
+	public Integer code() {
+		return code;
+	}
+	public void code(Integer code) {
+		this.code = code;
+	}
+	public void put(String key, Object value) {
+		this.data.put(key, value);
+	}
+	@SuppressWarnings("unchecked")
+	public <T> T get(String key) {
+		Object value = this.data.get(key);
+		if (value != null) {
+			return (T) value;
+		}
+		return null;
+	}
+	public Map<String, Object> asMap() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("success", this.success);
+		map.put("message", this.message);
+		map.put("code", this.code);
+		map.put("data", this.data);
+		return map;
+	}
+	public Map<String, Object> asMapWithout() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("success", this.success);
+		map.put("message", this.message);
+		map.put("code", this.code);
+		map.putAll(this.data);
+		return map;
+	}
+	public String asJSON() throws JsonProcessingException {
+		return new ObjectMapper().writeValueAsString(this.asMap());
+	}
+	public String asJSONWithout() throws JsonProcessingException {
+		return new ObjectMapper().writeValueAsString(this.asMapWithout());
 	}
 }
--- src/test/java/org/cuckoo/universal/utils/web/Main_ResponseEntity.java
+++ src/test/java/org/cuckoo/universal/utils/web/Main_ResponseEntity.java
@@ -1,7 +1,6 @@
 package org.cuckoo.universal.utils.web;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.time.LocalDate;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 
@@ -9,14 +8,39 @@ public class Main_ResponseEntity {
 
 	public static void main(String[] args) throws JsonProcessingException {
 
+		ResponseEntity re = test01();
+		System.out.println("success: "+re.success());
+		System.out.println("message: "+re.message());
+		System.out.println("code: "+re.code());
+		
+		// 自动类型转换
+		String name = re.get("user.name");
+		Integer age = re.get("user.age");
+		LocalDate birthday = re.get("user.birthday");
+		System.out.println("user.name: "+name);
+		System.out.println("user.age: "+age);
+		System.out.println("user.birthday: "+birthday.toString());
+		
+		// 转化为 JSON or Map
+		System.out.println(re.asJSON());
+		System.out.println(re.asJSONWithout());
+		System.out.println(re.asMap());
+		System.out.println(re.asMapWithout());
+		
+		// 直接输出JSON
 		System.out.println(ResponseEntity.create().success().code(200).message("操作成功").asJSON());
 		System.out.println(ResponseEntity.create().failure().code(400).message("操作失败，客户端异常").asJSON());
-		
-		Map<String, Object> msg = new HashMap<>();
-		msg.put("success", false);
-		msg.put("message", "操作失败，服务端异常");
-		msg.put("code", 500);
-		System.out.println(ResponseEntity.build().put(msg).asJSON());
+	}
+	
+	public static ResponseEntity test01() {
+		return ResponseEntity.create()
+				.success()
+				.code(200)
+				.message("ok")
+				.put("user.name", "Jim")
+				.put("user.age", 31)
+				.put("user.birthday", LocalDate.of(1988, 07, 28))
+				.as();
 	}
 
 }
